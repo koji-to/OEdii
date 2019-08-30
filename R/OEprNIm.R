f2974e34c932455e28542e08a55c35adb615ee83
Koji Toda <toda@fit.ac.jp>
2019-08-30 12:17:41 +0900

    Rename package
    Rename paclage: deletion -> elimination.
    Change some parameter names.

--- R/OEprNIm.R
+++ R/OEprNIm.R
@@ -1,9 +1,9 @@
-#' Delete outlier cases
+#' Eliminate outlier cases
 #'
 #' @param data.df target dataset
 #' @param imp imputation method name (mice (default) or missForest)
-#' @param missing_ratio ratio to product NA
-#' @param cut_off_ratio ratio of delete cases in whole data
+#' @param na_ratio ratio of producting NA
+#' @param co_ratio ratio of delete cases in whole data
 #' @param iter the number of of iteration
 #' @param calc_res calculation method of residual
 #'
@@ -13,13 +13,13 @@
 #' @importFrom missForest prodNA
 #'
 #' @examples
-#' ODproNIm(iris[,-ncol(iris)],iter=10)
+#' OEprNIm(iris[,-ncol(iris)],iter=10)
 #'
 #' @export
 
-ODproNIm<-function(data.df, imp="mice",missing_ratio=0.1 ,cut_off_ratio=0.2, iter=1000, calc_res="arm"){
+OEprNIm<-function(data.df, imp="mice", na_ratio=0.1 ,co_ratio=0.2, iter=1000, calc_res="arm"){
   for(i in 1:iter){
-    missing.df<-missForest::prodNA(data.df,noNA=missing_ratio)
+    missing.df<-missForest::prodNA(data.df,noNA=na_ratio)
     if(imp=="mice"){
       data_mice.mice<-mice::mice(missing.df,seed=i,m=1,printFlag=FALSE)
       imp.df<-mice::complete(data_mice.mice,1)
@@ -47,7 +47,7 @@ ODproNIm<-function(data.df, imp="mice",missing_ratio=0.1 ,cut_off_ratio=0.2, ite
   diff.df<-diff_sum.df/missing_count.df
   sum_diff.ls<-apply(diff.df,1,sum)
   data.df$rank<-rank(sum_diff.ls)
-  deleted.df<-subset(data.df,data.df$rank<=round(nrow(data.df)*(1-cut_off_ratio)))
+  deleted.df<-subset(data.df,data.df$rank<=round(nrow(data.df)*(1-co_ratio)))
 
   deleted.df<-deleted.df[,-ncol(deleted.df)]
   out.ls<-list(out_del=deleted.df,sum_diff=diff.df,miss_count=missing_count.df)
