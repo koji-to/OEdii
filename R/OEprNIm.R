84a38119d016dc5bf95538a83187bd7241fa5cf4
Koji Toda <toda@fit.ac.jp>
2019-08-30 12:47:21 +0900

    Rename variables
    Rename some variables and add @return to description.

--- R/OEprNIm.R
+++ R/OEprNIm.R
@@ -3,7 +3,7 @@
 #' @param data.df target dataset
 #' @param imp imputation method name (mice (default) or missForest)
 #' @param na_ratio ratio of producting NA
-#' @param co_ratio ratio of delete cases in whole data
+#' @param co_ratio ratio of eliminated cases from whole data
 #' @param iter the number of of iteration
 #' @param calc_res calculation method of residual
 #'
@@ -15,7 +15,9 @@
 #' @examples
 #' OEprNIm(iris[,-ncol(iris)],iter=10)
 #'
-#' @export
+#' @return $elim returns eliminated dataframe, $diff returns difference of each field and $mcont returns the number of missing count of each field
+#'
+#' @export OEprNIm
 
 OEprNIm<-function(data.df, imp="mice", na_ratio=0.1 ,co_ratio=0.2, iter=1000, calc_res="arm"){
   for(i in 1:iter){
@@ -47,9 +49,9 @@ OEprNIm<-function(data.df, imp="mice", na_ratio=0.1 ,co_ratio=0.2, iter=1000, ca
   diff.df<-diff_sum.df/missing_count.df
   sum_diff.ls<-apply(diff.df,1,sum)
   data.df$rank<-rank(sum_diff.ls)
-  deleted.df<-subset(data.df,data.df$rank<=round(nrow(data.df)*(1-co_ratio)))
+  elim.df<-subset(data.df,data.df$rank<=round(nrow(data.df)*(1-co_ratio)))
 
-  deleted.df<-deleted.df[,-ncol(deleted.df)]
-  out.ls<-list(out_del=deleted.df,sum_diff=diff.df,miss_count=missing_count.df)
+  elim.df<-elim.df[,-ncol(elim.df)]
+  out.ls<-list(elim=elim.df,diff=diff.df,mcount=missing_count.df)
   return(out.ls)
 }
