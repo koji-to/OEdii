3250ae45d5bf16aaab8dcc9e2592cfe88d2fa393
Koji Toda <toda@fit.ac.jp>
2020-10-08 16:57:12 +0900

    Chack and fix documents and code
    change parameter's names

--- R/OEdic.R
+++ R/OEdic.R
@@ -1,11 +1,11 @@
-#' Eliminate outlier cases by using generating NA and imputation chain.
+#' Eliminate outliers using deletion-imputation chain.
 #'
-#' @param data.df target dataset
-#' @param imp imputation method name ('mice' (default setting) or 'missForest')
-#' @param na_ratio ratio of generating NA
-#' @param co_ratio ratio of eliminated cases by whole data
+#' @param data.df outlier elimination target dataset
+#' @param imp imputation method's name ('mice' (default setting) or 'missForest')
+#' @param del_rate deletion rate to complete dataset
+#' @param elim_rate outlier elimination rate
 #' @param iter the number of of iteration
-#' @param calc_res calculation method of residual
+#' @param penl calculation method of residual
 #'
 #' @importFrom mice mice
 #' @importFrom mice complete
@@ -13,30 +13,36 @@
 #' @importFrom missForest prodNA
 #'
 #' @examples
-#' OEprNIm(iris[,-ncol(iris)],iter=10)
+#' OEdic(iris[,-ncol(iris)], iter=10)
+#' out <- OEdic(iris[,-ncol(iris)],iter=10)
+#' out$elim
 #'
-#' @return $elim returns eliminated dataframe, $diff returns difference of each field and $mcont returns the number of missing count of each field
+#' @return $elim returns dataframe without outlier (eliminated)
+#' 
+#' $diff returns means of difference between imputed value and actual one about each field
+#' 
+#' $mcont returns the number of missing count of each field
 #'
-#' @export OEprNIm
+#' @export OEdic
 
-OEprNIm<-function(data.df, imp="mice", na_ratio=0.1 ,co_ratio=0.2, iter=1000, calc_res="arm"){
+OEdic<-function(data.df, imp="mice", del_rate=0.1, elim_rate=0.2, iter=1000, penl="SQD"){
   for(i in 1:iter){
-    missing.df<-missForest::prodNA(data.df,noNA=na_ratio)
+    missing.df<-missForest::prodNA(data.df,noNA=del_rate)
     if(imp=="mice"){
       data_mice.mice<-mice::mice(missing.df,seed=i,m=1,printFlag=FALSE)
       imp.df<-mice::complete(data_mice.mice,1)
     }else if(imp=="missForst"){
       imp.df<-missForest::missForest(missing.df)$ximp
     }
-    if(calc_res=="arm"){#absolute residual mean
-      if(!exists("diff_sum.df")){
+    if(penl=="ABD"){
+      if(!exists("diff_sum.df")){#absolute difference
         diff_sum.df<-abs(data.df-imp.df)
         missing_count.df<-is.na(missing.df)
       }else{
         diff_sum.df<-diff_sum.df+abs(data.df-imp.df)
         missing_count.df<-missing_count.df+is.na(missing.df)
       }
-    }else if(calc_res=="rms"){#residual mean square
+    }else if(penl=="SQD"){#squared difference
       if(!exists("diff_sum.df")){
         diff_sum.df<-(data.df-imp.df)^2
         missing_count.df<-is.na(missing.df)
@@ -49,7 +55,7 @@ OEprNIm<-function(data.df, imp="mice", na_ratio=0.1 ,co_ratio=0.2, iter=1000, ca
   diff.df<-diff_sum.df/missing_count.df
   sum_diff.ls<-apply(diff.df,1,sum)
   data.df$rank<-rank(sum_diff.ls)
-  elim.df<-subset(data.df,data.df$rank<=round(nrow(data.df)*(1-co_ratio)))
+  elim.df<-subset(data.df,data.df$rank<=round(nrow(data.df)*(1-elim_rate)))
 
   elim.df<-elim.df[,-ncol(elim.df)]
   out.ls<-list(elim=elim.df,diff=diff.df,mcount=missing_count.df)
